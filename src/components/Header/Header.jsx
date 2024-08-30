996599b79e11fd8a623c3821bcd707de828a9945
cuijiawei123 <491758908@qq.com>
2024-08-30 15:45:48 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#people">Testimonials</a></li>
           <li className={`flexCenter ${css.phone}`}>
-            <p>+001 (313) 345 678</p>
+            <p>+86  15091753891</p>
             <BiPhoneCall size={"40px"} />
           </li>
         </ul>
